91f7df1daba7e43ef2f6efc5db2c8f9ddbd5e1ad
Divine Abhulimen <divoabhulimen@gmail.com>
2024-12-14 01:09:17 -0500

    Env Variables

--- src/config.js
+++ src/config.js
@@ -1,17 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDb6J7ksrFIJZMgeIvrkuQ_hzfOdrehgT4",
-  authDomain: "expenser-2335.firebaseapp.com",
-  projectId: "expenser-2335",
-  storageBucket: "expenser-2335.firebasestorage.app",
-  messagingSenderId: "829602228195",
-  appId: "1:829602228195:web:851194b850a6be96ee62d6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase

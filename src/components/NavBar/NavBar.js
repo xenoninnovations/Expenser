78b487fb90c0dc4e34b8528f5425a2eae986147f
Divine-Abhulimen <111998434+Divine-Abhulimen@users.noreply.github.com>
2025-01-28 12:19:42 -0500

    Navbar build

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./NavBar.css";
 import profileImg from "../../images/profile.png";
 import settingsIcon from "../../images/settings.svg";
@@ -13,126 +13,162 @@ export default function NavBar() {
   const [isBudgetingOpen, setIsBudgetingOpen] = useState(false);
   const [isTimeTrackingOpen, setIsTimeTrackingOpen] = useState(false);
   const [isBookKeepingOpen, setIsBookKeepingOpen] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(true);
 
-  const toggleBudgeting = () => {
-    setIsBudgetingOpen(!isBudgetingOpen);
-  };
+  // Auto-collapse on screen resize
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 2503) {
+        setIsNavExpanded(false);
+      } else {
+        setIsNavExpanded(true);
+      }
+    };
 
-  const toggleTimeTracking = () => {
-    setIsTimeTrackingOpen(!isTimeTrackingOpen);
-  };
+    handleResize();
+    window.addEventListener("resize", handleResize);
 
-  const toggleBookKeeping = () => {
-    setIsBookKeepingOpen(!isBookKeepingOpen);
-  };
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isNavExpanded ? "expanded" : "collapsed"}`}>
       <section className="profile-box">
-        {/* <img src={profileImg} alt="profile" className="profile-picture" /> */}
-        <section className="user-sett-dots">
-          <section className="username-settings">
-            <h3 className="username">Fizkies Floky</h3>
-            <img
-              src={settingsIcon}
-              alt="settings icon"
-              className="settings-icon"
-            />
+        <img
+          src={settingsIcon}
+          alt="settings icon"
+          className="settings-icon"
+          onClick={() => setIsNavExpanded(!isNavExpanded)}
+        />
+        {isNavExpanded && (
+          <section className="user-sett-dots">
+            <section className="username-settings">
+              <h3 className="username">Fizkies Floky</h3>
+              <img src={dots} alt="dots" className="dots" />
+            </section>
           </section>
-          <img src={dots} alt="dots" className="dots" />
-        </section>
+        )}
       </section>
+
       <ul className="nav-list">
-        <section className="list-dashboard">
-          <img src={dashboardIcon} alt="dashboard icon" className="list-icon" />
-          <a href="/"><li className="list-item">Dashboard</li></a>
-        </section>
         <section className="list-group">
-          <section className="main-nav" onClick={toggleBudgeting}>
+          <section className="main-nav">
+          <section className="icon-item">
+            <img
+              src={dashboardIcon}
+              alt="dashboard icon"
+              className="list-icon"
+            />
+            {isNavExpanded && (
+              <a href="/">
+                <li className="list-item">Dashboard</li>
+              </a>
+            )}
+          </section>
+          </section>
+        </section>
+
+        {/* Budgeting Section */}
+        <section className="list-group">
+          <section
+            className="main-nav"
+            onClick={() => setIsBudgetingOpen(!isBudgetingOpen)}
+          >
             <section className="icon-item">
               <img
                 src={budgetingIcon}
                 alt="budgeting icon"
                 className="list-icon"
               />
-              <li className="list-item">Budgeting</li>
+              {isNavExpanded && <li className="list-item">Budgeting</li>}
             </section>
-            <img
-              src={collapseIcon}
-              alt="collapse icon"
-              className={`collapse-icon ${isBudgetingOpen ? "open" : ""}`}
-            />
+            {isNavExpanded && (
+              <img
+                src={collapseIcon}
+                alt="collapse icon"
+                className={`collapse-icon ${isBudgetingOpen ? "open" : ""}`}
+              />
+            )}
           </section>
-          {isBudgetingOpen && (
+          {isBudgetingOpen && isNavExpanded && (
             <ul className="mini-nav">
-              <a href="/expensetracker"><li className="mini-item">Expense Tracker</li></a>
+              <li className="mini-item">
+                <a href="/expensetracker">Expense Tracker</a>
+              </li>
               <li className="mini-item">Income/Revenue</li>
               <li className="mini-item">Savings</li>
               <li className="mini-item">Transactions</li>
             </ul>
           )}
         </section>
+
+        {/* Time Tracking Section */}
         <section className="list-group">
-          <section className="main-nav" onClick={toggleTimeTracking}>
+          <section
+            className="main-nav"
+            onClick={() => setIsTimeTrackingOpen(!isTimeTrackingOpen)}
+          >
             <section className="icon-item">
               <img
                 src={timeIcon}
                 alt="time tracking icon"
                 className="list-icon"
               />
-              <li className="list-item">Time tracking</li>
+              {isNavExpanded && <li className="list-item">Time Tracking</li>}
             </section>
-            <img
-              src={collapseIcon}
-              alt="collapse icon"
-              className={`collapse-icon ${isTimeTrackingOpen ? "open" : ""}`}
-            />
+            {isNavExpanded && (
+              <img
+                src={collapseIcon}
+                alt="collapse icon"
+                className={`collapse-icon ${isTimeTrackingOpen ? "open" : ""}`}
+              />
+            )}
           </section>
-          {isTimeTrackingOpen && (
+          {isTimeTrackingOpen && isNavExpanded && (
             <ul className="mini-nav">
-              <li className="mini-item"><a href="/timetracker">Time Tracker</a></li>
+              <li className="mini-item">
+                <a href="/timetracker">Time Tracker</a>
+              </li>
               <li className="mini-item">Calendar</li>
               <li className="mini-item">Automations</li>
               <li className="mini-item">Time Sheets</li>
             </ul>
           )}
         </section>
+
+        {/* Book Keeping Section */}
         <section className="list-group">
-          <section className="main-nav" onClick={toggleBookKeeping}>
+          <section
+            className="main-nav"
+            onClick={() => setIsBookKeepingOpen(!isBookKeepingOpen)}
+          >
             <section className="icon-item">
               <img
                 src={bookIcon}
                 alt="bookkeeping icon"
                 className="list-icon"
               />
-              <li className="list-item">Book keeping</li>
+              {isNavExpanded && <li className="list-item">Book Keeping</li>}
             </section>
-            <img
-              src={collapseIcon}
-              alt="collapse icon"
-              className={`collapse-icon ${isBookKeepingOpen ? "open" : ""}`}
-            />
+            {isNavExpanded && (
+              <img
+                src={collapseIcon}
+                alt="collapse icon"
+                className={`collapse-icon ${isBookKeepingOpen ? "open" : ""}`}
+              />
+            )}
           </section>
-          {isBookKeepingOpen && (
+          {isBookKeepingOpen && isNavExpanded && (
             <ul className="mini-nav">
-              <a href="/clientmanagement"><li className="mini-item">Client Management</li></a>
+              <li className="mini-item">
+                <a href="/clientmanagement">Client Management</a>
+              </li>
               <li className="mini-item">Cost Breakdown</li>
               <li className="mini-item">Invoicing & Payments</li>
-              <li className="mini-item">Transaction Tracking</li>
-              <li className="mini-item">Reports & Analytics</li>
-              <li className="mini-item">Tax Preparation</li>
-              <li className="mini-item">Recurring Entries</li>
             </ul>
           )}
         </section>
       </ul>
-
-      <div>
-        <section id="auth">
-          <a href="/signin">Signin</a> <br/>
-          <a href="/signup">Signup</a>
-        </section>
-      </div>
     </nav>
   );
 }
